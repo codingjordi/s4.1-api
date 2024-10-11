05c705309af3f38a85d0af7e011416415f4a9bca
codingjordi <jordisaperasc@gmail.com>
2024-10-11 22:06:09 +0200

    s4.1-api-nivell-1-exercici-3

--- main.js
+++ main.js
@@ -1,21 +1,47 @@
+var reportAcudits = [];
+var currentJoke = '';
+var options = {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json'
+    }
+};
+var apiURL = 'https://icanhazdadjoke.com/';
 function showRandomJoke() {
     var jokeP = document.getElementById('random-joke');
-    var options = {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json'
-        }
-    };
-    var apiUrl = 'https://icanhazdadjoke.com/';
-    fetch(apiUrl, options)
+    fetch(apiURL, options)
         .then(function (res) {
         if (!res.ok) {
             throw new Error("Something went wrong and it couldn't retrieve the joke.... And that's not fun :-(");
         }
-        console.log(typeof res);
         return res.json();
     })
         .then(function (data) {
-        jokeP === null || jokeP === void 0 ? void 0 : jokeP.innerText = data.joke;
+        currentJoke = data.joke;
+        jokeP.innerText = currentJoke;
+        if (!reportAcudits.some(function (joke) { return joke.joke === currentJoke; })) {
+            reportAcudits.push({
+                joke: data.joke,
+                score: 0,
+                date: new Date().toISOString()
+            });
+        }
     })["catch"](function (e) { return console.log(e); });
+}
+function rateJoke(points) {
+    var jokeToRate = reportAcudits.find(function (joke) {
+        return joke.joke === currentJoke;
+    });
+    if (jokeToRate) {
+        jokeToRate.score = points;
+        console.log(reportAcudits);
+        showVoteModal(); // Muestra el modal al votar
+    }
+}
+function showVoteModal() {
+    var modal = document.getElementById('vote-modal');
+    modal === null || modal === void 0 ? void 0 : modal.classList.add('show'); // Muestra el modal
+    setTimeout(function () {
+        modal === null || modal === void 0 ? void 0 : modal.classList.remove('show'); // Lo oculta después de 1.5 segundos
+    }, 1500);
 }
--- main.ts
+++ main.ts
@@ -1,27 +1,60 @@
+let reportAcudits : Array<{joke: String, score: Number, date: String}> = [] ;
+let currentJoke = '';
+
+const options : Object = {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json'
+    }
+}
+
+const apiURL = 'https://icanhazdadjoke.com/';
+
 function showRandomJoke() {
 
     const jokeP = document.getElementById('random-joke')
 
-    const options : Object = {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json'
-        }
-    }
-
-    const apiUrl = 'https://icanhazdadjoke.com/';
-    fetch(apiUrl, options)
+    
+    fetch(apiURL, options)
         .then((res) => {
             if (!res.ok) {
-
                 throw new Error("Something went wrong and it couldn't retrieve the joke.... And that's not fun :-(")
             }
             return res.json()
         })
         .then((data) => {
-            jokeP?.innerText = data.joke
+            currentJoke = data.joke
+            jokeP.innerText = currentJoke
+            
+            if(!reportAcudits.some((joke) => joke.joke === currentJoke)) {
+                reportAcudits.push({
+                    joke: data.joke,
+                    score: 0,
+                    date: new Date().toISOString()
+                })
+            }
         })
         .catch((e) => console.log(e))
+}
 
-    
+
+function rateJoke(points: Number) {
+    const jokeToRate = reportAcudits.find(joke => {
+        return joke.joke === currentJoke;
+    });
+
+    if (jokeToRate) {
+        jokeToRate.score = points;
+        console.log(reportAcudits);
+        showVoteModal(); // Muestra el modal al votar
+    }
+}
+
+
+function showVoteModal() {
+    const modal = document.getElementById('vote-modal');
+    modal?.classList.add('show'); // Muestra el modal
+    setTimeout(() => {
+        modal?.classList.remove('show'); // Lo oculta después de 1.5 segundos
+    }, 1500);
 }
